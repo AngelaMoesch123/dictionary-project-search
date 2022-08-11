bfc82d66372d95d03fbb534b5a3df4d2d8cf6f62
AngelaMoesch123 <angelarosesnyder08@gmail.com>
2022-08-11 13:03:17 -0700

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>
+            This project was created by Angela Moesch and is{" "}
+            <a
+              href="https://github.com/AngelaMoesch123/dictionary-project-search"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+            , and hosted on{" "}
+            <a
+              href="https://adorable-pasca-0b0796.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
